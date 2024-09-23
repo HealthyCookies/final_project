81f0b93d506c20509e1dcf400090c4556555c087
Damncaf-goin-crazy <Decafattic@gmail.com>
2024-09-23 18:04:22 +0300

    Filling database on launch added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:final_project/use_cases/first_launch_use_case.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app_router/app_router.dart';
 import 'common/providers/shared_preferences_provider.dart';
+import 'features/main/data/activityDbService.dart';
+import 'features/main/data/foodDbService.dart';
+import 'features/main/data/ingredientDbService.dart';
+import 'features/main/data/mealDbService.dart';
 import 'themes/theme_notifier.dart';
 import 'themes/themes.dart';
 
@@ -11,6 +16,21 @@ void main() async {
 
   final SharedPreferences prefs = await SharedPreferences.getInstance();
 
+  final ActivityDbService activityDbService = ActivityDbService();
+  final FoodDbService foodDbService = FoodDbService();
+  final IngredientDbService ingredientDbService = IngredientDbService();
+  final MealDbService mealDbService = MealDbService();
+
+  final FirstLaunchUseCase firstLaunchUseCase = FirstLaunchUseCase(
+    prefs,
+    activityDbService,
+    foodDbService,
+    ingredientDbService,
+    mealDbService,
+  );
+
+  await firstLaunchUseCase.execute();
+
   runApp(ProviderScope(
     overrides: <Override>[
       sharedPreferencesProvider.overrideWithValue(prefs),
@@ -18,7 +38,6 @@ void main() async {
     child: MyApp(),
   ));
 }
-
 class MyApp extends ConsumerWidget {
   MyApp({super.key});
 

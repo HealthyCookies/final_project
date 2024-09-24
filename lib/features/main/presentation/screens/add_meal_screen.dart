38e9cd631bebc949e2af859d90d9bfed9363b4fc
mihdenis85 <mihdenis777@yandex.ru>
2024-09-24 14:28:41 +0300

    fix: requested edits

--- lib/features/main/presentation/screens/add_meal_screen.dart
+++ lib/features/main/presentation/screens/add_meal_screen.dart
@@ -3,17 +3,79 @@ import 'dart:developer';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bounceable/flutter_bounceable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../common/dialogs/show_confirm_dialog.dart';
 import '../../../../common/widgets/default_sliver_app_bar.dart';
 import '../../../../common/widgets/default_text_form_filed.dart';
 
 @RoutePage()
-class AddMealScreen extends StatefulWidget {
+class AddMealScreen extends ConsumerWidget {
   const AddMealScreen({super.key});
 
   @override
-  AddMealScreenState createState() => AddMealScreenState();
+  Widget build(BuildContext context, WidgetRef ref) {
+    MealType? selectedMealType;
+
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        slivers: <Widget>[
+          DefaultSliverAppBar(
+            leading: Bounceable(
+              onTap: context.maybePop,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: const Icon(Icons.arrow_back),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Column(
+                children: <Widget>[
+                  const Text(
+                    'Input the dish name, its type (Breakfast, Lunch, Dinner) and Calories',
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 40.0),
+                  const DefaultTextFormFiled(
+                    labelText: 'Name',
+                  ),
+                  const SizedBox(height: 40.0),
+                  MealTypeDropdown(
+                    initialType: selectedMealType,
+                    onTypeChanged: (MealType? value) {
+                      selectedMealType = value;
+                    },
+                  ),
+                  const SizedBox(height: 40.0),
+                  const DefaultTextFormFiled(
+                    labelText: 'Kilocalories',
+                    inputType: TextInputType.number,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.check),
+        onPressed: () async {
+          if (await showConfirmDialog(context)) {
+            log('Implement saving');
+            if (context.mounted) {
+              context.maybePop();
+            }
+          }
+        },
+      ),
+    );
+  }
 }
 
 enum MealType {
@@ -93,73 +155,4 @@ class _MealTypeDropdownState extends State<MealTypeDropdown> {
     );
   }
 }
-
-class AddMealScreenState extends State<AddMealScreen> {
-  MealType? selectedMealType;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        physics: const NeverScrollableScrollPhysics(),
-        slivers: <Widget>[
-          DefaultSliverAppBar(
-            leading: Bounceable(
-              onTap: context.maybePop,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: const Icon(Icons.arrow_back),
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Column(
-                children: <Widget>[
-                  const Text(
-                    'Input the dish name, its type (Breakfast, Lunch, Dinner) and Calories',
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 40.0),
-                  const DefaultTextFormFiled(
-                    labelText: 'Name',
-                  ),
-                  const SizedBox(height: 40.0),
-                  MealTypeDropdown(
-                    initialType: selectedMealType,
-                    onTypeChanged: (MealType? value) {
-                      setState(() {
-                        selectedMealType = value;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 40.0),
-                  const DefaultTextFormFiled(
-                    labelText: 'Kilocalories',
-                    inputType: TextInputType.number,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.check),
-        onPressed: () async {
-          if (await showConfirmDialog(context)) {
-            log('Implement saving');
-            if (context.mounted) {
-              context.maybePop();
-            }
-          }
-        },
-      ),
-    );
-  }
-}
-
 

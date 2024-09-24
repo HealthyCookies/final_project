9c2edee601ccc23cea7eb639d1ac739787ca2a82
Adel Shagaliev <a.shagaliev@innopolis.university>
2024-09-24 19:51:15 +0300

    feat: added skeleton for MealListScreen

--- lib/features/all_meals/presentation/screens/all_meals_screen.dart
+++ lib/features/all_meals/presentation/screens/all_meals_screen.dart
@@ -3,30 +3,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../common/widgets/default_sliver_app_bar.dart';
+import '../../../../domain/use_cases/load_meals.dart';
+import '../state_notifiers/meals_list_notifier.dart';
 
 @RoutePage()
 class AllMealsScreen extends ConsumerWidget {
-  @RoutePage()
   const AllMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
+    return const Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          const DefaultSliverAppBar(title: Text('Meals List')),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Text('Meal $index'),
-                );
-              },
-              childCount: 10,
-            ),
-          ),
+          DefaultSliverAppBar(title: Text('Meals List')),
+          _MealsList(),
         ],
+      ),
+    );
+  }
+}
+
+class _MealsList extends ConsumerStatefulWidget {
+  const _MealsList();
+
+  @override
+  ConsumerState<_MealsList> createState() => __MealsListState();
+}
+
+class __MealsListState extends ConsumerState<_MealsList> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref
+          .read(mealsListStateNotifierProvider.notifier)
+          .loadMeals(const LoadMealsParams());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final MealsListState state = ref.watch(mealsListStateNotifierProvider);
+
+    if (state.meals.isEmpty) {
+      return const SliverToBoxAdapter(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return SizedBox(
+            child: Text(state.meals[index].name),
+          );
+        },
+        childCount: state.meals.length,
       ),
     );
   }
